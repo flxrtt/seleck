eda189cb9e3de88265366d27213d3bb44f9d2a05
flxrtt <147021368+flxrtt@users.noreply.github.com>
2023-10-05 00:38:48 -0400

    Update moneyesp.lua

--- moneyesp.lua
+++ moneyesp.lua
@@ -1,14 +1,10 @@
--- Get the service for name tags
-local NameTagService = game:GetService("NameTagService")
+local players = game.Players:GetChildren()
 
--- Get all the name tags in the game
-local NameTags = NameTagService:GetAllNameTags()
+for i, player in pairs(players) do
+  local highlight = Instance.new("Highlight")
+  highlight.Adornee = player.Character
+  highlight.Parent = player.Character
 
--- Loop through all the name tags
-for i, NameTag in pairs(NameTags) do
-  -- Get the player associated with the name tag
-  local Player = NameTag:GetPlayer()
-
-  -- Highlight the player
-  Player.Character.HumanoidRootPart.Highlight = true
+  highlight.OutlineColor = Color3.new(0, 1, 0)
+  highlight.Transparency = 0.5
 end
